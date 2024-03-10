5a72363f68949b1624e615539826c6c3b861d181
sumafujiko <sumafujiko@gmail.com>
2024-03-10 21:28:25 +0000

    Included grading system

--- Averageresult.java
+++ Averageresult.java
@@ -8,6 +8,7 @@ public class Averageresult {
     public static void main(String[] args) { 
         //Declare variables
         int mark = 0;
+        int totalMark = 0;
         int subjectInput = Integer.parseInt(JOptionPane.showInputDialog("How many subjects do you take?"));
         //Check if the user has entered a valid number of subjects
         if (subjectInput < 2) {
@@ -17,15 +18,26 @@ public class Averageresult {
             JOptionPane.showMessageDialog(null, "You can only take a maximum of 10 subjects");
             System.exit(0);
         }
-        int totalMark = 0;
         //Loop to get the marks for each subject
         for (int i=0; i<subjectInput; i++) {
             mark = Integer.parseInt(JOptionPane.showInputDialog("Enter your mark for subject " + (i + 1)));
             totalMark += mark;
         }
         //Calculate the average mark
-        double average = totalMark / subjectInput;
-        //Output the average mark
-        JOptionPane.showMessageDialog(null, "Your average mark is " + average);
+        int average = totalMark / subjectInput;
+        //Calculate and output the grade and mark
+        if (average >= 90) {
+            JOptionPane.showMessageDialog(null, "Your average mark is " + average + " which is an A");
+        } else if (average >= 80) {
+            JOptionPane.showMessageDialog(null, "Your average mark is " + average + " which is a B");
+        } else if (average >= 70) {
+            JOptionPane.showMessageDialog(null, "Your average mark is " + average + " which is a C");
+        } else if (average >= 60) {
+            JOptionPane.showMessageDialog(null, "Your average mark is " + average + " which is a D");
+        } else if (average >= 50) {
+            JOptionPane.showMessageDialog(null, "Your average mark is " + average + " which is an E");
+        } else {
+            JOptionPane.showMessageDialog(null, "Your average mark is " + average + " which is an F");
+        }
     }
 }
